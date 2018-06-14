2146ead1a0380707286a7034634a0e7fde317122
Frank.Chen <mikru168@gmail.com>
2018-06-14 23:48:39 +0800

    - First commit

---
+++ PageViewControllerDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PageViewControllerDemo
+//
+//  Created by 陳韋全 on 2018/6/11.
+//  Copyright © 2018年 陳韋全. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
